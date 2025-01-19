5057a3c3084f4ac6568478ba5f8aaf1ab5ab7607
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-01-19 22:18:39 +0530

    Font style improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_flow/screens/home_screen.dart';
 
 void main() {
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 29, 58, 145)),
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(),
         useMaterial3: true,
       ),
       home: const HomeScreen(),
